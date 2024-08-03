7fc2e3d353e3e688c8decab727e31d3f4cdc1b13
ryolingo <ryomatsuya04@gmail.com>
2024-08-03 15:29:02 +0900

    second commit

---
+++ src/app/api/blog/[id]/route.ts
@@ -0,0 +1,58 @@
+import { NextResponse } from "next/server";
+import { main } from "../route";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+//ブログの詳細記事取得用API
+export const GET = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+    await main();
+    const post = await prisma.post.findFirst({ where: { id } }); //http/localhost:3000/api/blog/3
+    return NextResponse.json({ message: "success", post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+//ブログ編集用API
+export const PUT = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+
+    const { title, description } = await req.json();
+
+    await main();
+    const post = await prisma.post.update({
+      data: { title, description },
+      where: { id },
+    }); //http/localhost:3000/api/blog/3
+    return NextResponse.json({ message: "success", post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
+
+//削除用API
+export const DELETE = async (req: Request, res: NextResponse) => {
+  try {
+    const id: number = parseInt(req.url.split("/blog/")[1]);
+
+    const { title, description } = await req.json();
+
+    await main();
+    const post = await prisma.post.delete({
+      where: { id },
+    }); //http/localhost:3000/api/blog/3
+    return NextResponse.json({ message: "success", post }, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({ message: "Error", err }, { status: 500 });
+  } finally {
+    await prisma.$disconnect();
+  }
+};
